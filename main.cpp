500c3a8a7b33ab085abdcafc21eb1ed3d2aa6120
xTheCoderx <justaguyoney@gmail.com>
2021-10-16 14:56:13 +1300

    Added RandomNumberGenerator class.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+
+class RandomNumberGenerator {
+public:
+    // NAWAZ'S STACKOVERFLOW CODE LINK: https://stackoverflow.com/questions/7560114/random-number-c-in-some-range
+    static int Random(const int min, const int max) {
+        static bool first = true;
+        if (first) {
+            srand( time(nullptr) );
+            first = false;
+        }
+        return min + rand() % (( max + 1 ) - min);
+    }
+};
 
 std::string ScrambleString(std::string &str) {
-
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string str = "Hi";
+    ScrambleString(str);
+
     return 0;
 }
